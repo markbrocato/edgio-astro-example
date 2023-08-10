23d01c9d93852c16bb4550c347265925cee7cb59
Mark Brocato <mbrocato@edg.io>
2023-08-10 16:56:41 +0300

    initial commit

---
+++ routes.ts
@@ -0,0 +1,10 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+import { Router } from "@edgio/core";
+import { astroRoutes } from "@edgio/astro";
+
+export default new Router()
+  .use(astroRoutes)
+  .get("/_astro/:path*", ({ serveStatic }) => {
+    serveStatic("dist/client/_astro/:path*");
+  });
